d98a749b468c980061e2ff9515c9f4716a768d2f
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-12-05 16:06:34 -0500

    feat: combine speech synth w/ dom update

--- angry-parent-simulator/js/main.js
+++ angry-parent-simulator/js/main.js
@@ -1,32 +1,20 @@
-document.querySelector('#yell').addEventListener('click', run)
+const synth = window.speechSynthesis;
+document.querySelector("#yell").addEventListener("click", run);
 
 function run() {
-  const fName = document.querySelector('#firstName').value
-  const fMidName = document.querySelector('#firstMiddle').value
-  const lMidName = document.querySelector('#lastMiddle').value
-  const lName = document.querySelector('#lastName').value
-
-  document.querySelector('#placeToYell').innerText = fName + ' ' + fMidName + ' ' + ' ' + lMidName + ' ' + lName
+  const fName = document.querySelector("#firstName").value;
+  const fMidName = document.querySelector("#firstMiddle").value;
+  const lMidName = document.querySelector("#lastMiddle").value;
+  const lName = document.querySelector("#lastName").value;
 
   //Add what you should be doing - conditionals go here
-
-  // document.querySelector('#placeToYell').innerText = `${fName} ${fMidName} ${lMidName} ${lName}`
+  if (fName !== "" || fMidName !== "" || lMidName !== "" || lName !== "") {
+    const yellText = `Yaa done messed up, ${fName} ${fMidName} ${lMidName} ${lName}`;
+    document.querySelector("#placeToYell").innerText = yellText;
+    synth.speak(new SpeechSynthesisUtterance(yellText));
+  } else {
+    document.querySelector("#placeToYell").innerText =
+      "GENERIC YELLING!  (enter your name for a personal scolding)";
+    synth.speak(new SpeechSynthesisUtterance("I'm yelling at you!"));
+  }
 }
-
-// const synth = window.speechSynthesis;
-// document.querySelector('#yell').addEventListener('click', run)
-//
-// function run() {
-//   const fName = document.querySelector('#firstName').value
-//   const fMidName = document.querySelector('#firstMiddle').value
-//   const lMidName = document.querySelector('#lastMiddle').value
-//   const lName = document.querySelector('#lastName').value
-//
-//   const yellText =  `${fName} ${fMidName} ${lMidName} ${lName}`
-//
-//   document.querySelector('#placeToYell').innerText = yellText
-//
-//   let yellThis = new SpeechSynthesisUtterance(yellText);
-//
-//   synth.speak(yellThis);
-// }
