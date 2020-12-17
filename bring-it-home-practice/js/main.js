469fb6cc0b5cad22b4c73a2062a9c1cb0f69f9c4
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-12-17 12:12:40 -0500

    feat: complete function exercises

--- bring-it-home-practice/js/main.js
+++ bring-it-home-practice/js/main.js
@@ -5,12 +5,20 @@ console.log(variable);
 
 // Create a variable, add 10 to it, and alert the value
 let anotherVariable = 100;
-alert(anotherVariable + 10);
+//alert(anotherVariable + 10);
 
 // *Functions*
 // Create a function that subtracts 4 numbers and alerts the difference
+function subtractFourNumbers(minuend, x, y, z) {
+  alert(minuend - x - y - z);
+}
+//subtractFourNumbers(15, 5, 2, 1);
 
 // Create a function that divides one number by another and returns the remainder
+function getRemainder(dividend, divisor) {
+  return dividend % divisor;
+}
+console.log(getRemainder(209, 23));
 
 // *Conditionals*
 // Create a function that adds two numbers and if the sum is greater than 50 alert Jumanji
