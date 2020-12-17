b4bd8c5d6d79b94492d26c1a26a1ef14abdec6ea
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-12-17 12:05:31 -0500

    feat; create addition functions

--- simple-functions/js/main.js
+++ simple-functions/js/main.js
@@ -1,5 +1,19 @@
 //create a function that adds two numbers and alerts the sum
+function addTwoNumbers(x, y) {
+  alert(x + y);
+}
 
+// extended add function that takes a list of numbers and returns their sum
+function add(...rest) {
+  const sum = rest.reduce((acc, number) => {
+    return (acc += number);
+  }, 0);
+  return sum;
+}
 //create a function that multiplys three numbers and console logs the product
 
 //create a function that divides two numbers and returns the ???
+
+// Test Cases, uncomment to test
+
+//addTwoNumbers(5, 6);
