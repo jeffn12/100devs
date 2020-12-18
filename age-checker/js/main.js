bc58ae6ce5059f090f5f9a0c8caef437764ff1f1
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-12-17 20:32:02 -0500

    feat: add age checker

--- age-checker/js/main.js
+++ age-checker/js/main.js
@@ -6,8 +6,35 @@
 //If under 30, tell them they can not rent fancy cars affordably
 //If under over 30, tell them there is nothing left to look forward too
 
-
 //--- Harder
 //On click of the h1
 //Take the value from the input
 //Place the result of the conditional in the paragraph
+const h1El = document.querySelector('h1');
+const inputEl = document.querySelector('input');
+const pEl = document.querySelector('p');
+
+h1El.addEventListener('click', evaluate);
+function evaluate() {
+  let age = inputEl.value;
+  let response = '';
+  if (age < 16) {
+    response += "you can't drive\n";
+  }
+  if (age < 18) {
+    response +=
+      "you can't hate from outside the club, cause you can't even get in\n";
+  }
+  if (age < 21) {
+    response += "you can't drink\n";
+  }
+  if (age < 21) {
+    response += "you can't rent cars affordably\n";
+  }
+  if (age < 21) {
+    response += "you can't rent fancy cars affordably\n";
+  } else {
+    response += 'there is nothing left to look forward to :-(\n';
+  }
+  pEl.innerText = response;
+}
